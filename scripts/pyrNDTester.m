9d076d880c6aa7a8b0b0bc8f3d2e7e9f208acd79
neckenstein <nick.eckenstein@gmail.com>
2019-05-25 01:17:36 -0400

    Syntax fixes to pyrNDTester and calculateCellSize

--- scripts/pyrNDTester.m
+++ scripts/pyrNDTester.m
@@ -4,4 +4,4 @@ DesignParams.rotationCenter=0;
 CompParams.resolution=10;
 CompParams.tolerance=0.01;
 CompParams.dimensions=[1 2 3 4];
-[aaTest2,onaaTest2,onaaDigits2,ndMetrics2]=pyramidWsND([1 2 3 4],DesignParams,CompParams);
+[aaTest2,onaaTest2,onaaDigits2,ndMetrics2]=pyramidWsND(DesignParams,CompParams);
--- source/calculateCellSize.m
+++ source/calculateCellSize.m
@@ -1,9 +1,9 @@
 function cellSize = calculateCellSize(Params)
 cellSize=1;
 for i=1:numel(Params.dimensions)
-    if CompParams.dimensions(i)<3
+    if Params.dimensions(i)<3
         cellSize=cellSize*4/Params.resolution;
-    elseif CompParams.dimensions(i)>=3
+    elseif Params.dimensions(i)>=3
         cellSize=cellSize*pi/Params.resolution;
     end
 end
