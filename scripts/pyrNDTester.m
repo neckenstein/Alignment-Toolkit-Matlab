9bee4ff5008ab6fbeda5772b91f20b6ae2cc2b7a
neckenstein <nick.eckenstein@gmail.com>
2019-06-11 13:44:50 -0400

    updates to readme

--- scripts/pyrNDTester.m
+++ scripts/pyrNDTester.m
@@ -1,9 +1,9 @@
 clear all
 Params.aspectRatio=1/2;
 Params.rotationCenter=0;
-Params.resolution=10;
+Params.resolution=20;
 Params.tolerance=0.01;
-Params.dimensions=[1 2 3 4 5];
+Params.dimensions=[1 3 4];
 
 connPair=get3DPyramidConnector(Params);
 [aaTest2,onaaTest2,onaaDigits2,ndMetrics2]=aaByFloodND(connPair,Params);
