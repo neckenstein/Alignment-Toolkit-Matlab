e144cab7d4f2a30eb8db7705110a349c534ad909
neckenstein <nick.eckenstein@gmail.com>
2019-09-13 00:05:33 -0400

    filter testing

---
+++ source/findWsVFaceDepthFilterTest.m
@@ -0,0 +1,89 @@
+function [sumAA,onAA,onAACells,mergeMetrics]=findWsVFaceDepth(resolution,DesignParams, depthToleranceFactor)
+FALLOFF_MINIMUM=0;
+BORDER_SIZE=100;
+FILE_CHARS=24;
+if mod(resolution,2)>0
+    resolution=resolution-1;
+end
+
+filename=makeFilename(DesignParams);
+
+Offsets=populate2DOffsets(resolution);
+
+ObstaclePolyhedra=generate2DObstaclePolyhedra(DesignParams);
+
+heightMap=generateHeightMap(Offsets,ObstaclePolyhedra);
+% figure(4)
+% surf(heightMap)
+% heightMap=filterHeightMap(heightMap);
+% figure(5)
+% surf(heightMap)
+depthTolerance=findDepthTolerance(depthToleranceFactor,heightMap);
+
+watersheds=watershed(heightMap)+2;
+PlotLimits=find2DPlotLimits(watersheds,BORDER_SIZE,Offsets);
+
+plotOpaque3DHeightMap(watersheds,heightMap,Offsets,PlotLimits,DesignParams,resolution)
+
+mergeMetrics=populate2DMergeMetrics(heightMap,watersheds);
+
+watersheds=merge_watersheds_by_depth(heightMap,watersheds,depthTolerance);
+
+watersheds=merge_dams(heightMap,watersheds);
+
+%Change super-low points so plot is more readable
+heightMap(heightMap<FALLOFF_MINIMUM)=FALLOFF_MINIMUM;
+currentFilepath=mfilename('fullpath');
+rootPath=currentFilepath(1:end-FILE_CHARS);
+dataFilepath=strcat(rootPath,'\data\discrete2dvfacefigs\',filename);
+plotOpaque3DHeightMap(watersheds,heightMap,Offsets,PlotLimits,DesignParams,resolution)
+saveas(figure(1),strcat(dataFilepath,'.jpg'))
+plotTransparent3DHeightMap(watersheds,heightMap,Offsets,PlotLimits,DesignParams,resolution)
+saveas(figure(2),strcat(dataFilepath,'_alpha.jpg'))
+
+%Flat image
+plot2DAA(watersheds)
+saveas(figure(3),strcat(dataFilepath,'_imagesc.jpg'))
+
+cellSize=(4*pi)/(resolution)^2;
+sumAA=findSumAA(watersheds,cellSize);
+[onAA,onAACells]=findONAA(watersheds,resolution);
+
+end
+
+function [filename]=makeFilename(designParams)
+filename = strcat('vface_flooding_ar',...
+    num2str(designParams.aspectRatio*100),...
+    '_cor',num2str(designParams.rotationCenter*100));
+end
+
+function plot2DAA(watersheds)
+figure(3)
+imagesc(watersheds)
+xlabel('x')
+ylabel('theta')
+set(gca,'Ydir','normal')
+end
+
+function [filteredHMap] = filterHeightMap(hMap)
+    filteredHMap =imgaussfilt(hMap);
+end
+
+function [PlotLimits]=find2DPlotLimits(watersheds,borderSize,Offsets)
+[iWatersheds,jWatersheds]=find(watersheds~=1);
+PlotLimits.iMin=max(min(iWatersheds)-borderSize,1);
+PlotLimits.iMax=min(max(iWatersheds)+borderSize,size(Offsets.xMat,1));
+PlotLimits.jMin=max(min(jWatersheds)-borderSize,1);
+PlotLimits.jMax=min(max(jWatersheds)+borderSize,size(Offsets.xMat,2));
+end
+function [sumAA]=findSumAA(watersheds,cellSize)
+aaMatching=pointsInAA(watersheds);
+wsAreaPoints=nnz(aaMatching);
+sumAA=wsAreaPoints*cellSize;
+end
+
+
+
+
+
+
