614192bc0c001f382b8323c992d04fac430dda21
neckenstein <nick.eckenstein@gmail.com>
2019-09-13 18:31:38 -0400

    fix to populate2Dmergemetrics

--- source/populate2DMergeMetrics.m
+++ source/populate2DMergeMetrics.m
@@ -3,8 +3,8 @@ watershedIds=unique(watersheds);
 mergeMetrics=zeros(numel(watershedIds)-1,5); %'Merge metrics' and neighbors
 for i=2:numel(watershedIds)
     [neighbor, depth]=find_ws_depth(heightMap,watersheds,watershedIds(i));
-    [neighbor, volume]=find_ws_volume(heightMap,watersheds,watershedIds(i));
+    [neighbor2, volume]=find_ws_volume(heightMap,watersheds,watershedIds(i));
     surface = nnz(watersheds==watershedIds(i));    
-    mergeMetrics(i-1,:)=[watershedIds(i) neighbor depth surface volume];
+    mergeMetrics(i-1,:)=[double(watershedIds(i)) double(neighbor) depth surface volume];    
 end
 end
