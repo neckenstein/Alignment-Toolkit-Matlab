92ea9adee3d1b0dc0bcf326c3da31e01e4ed8a79
neckenstein <geaelith@gmail.com>
2019-11-02 17:17:29 -0400

    final parameters matching tests

--- scripts/pyrNDTester.m
+++ scripts/pyrNDTester.m
@@ -1,10 +1,10 @@
 clear all
 %Parameters can vary across multiple simulations
 Params.aspectRatio=1/2;
-Params.rotationCenter=0;
-Params.resolution=20;
+Params.rotationCenter=-1.31;
+Params.resolution=60;
 Params.tolerance=0.01;
-Params.dimensions=[1 2 3];
+Params.dimensions=[1 4 5];
 %Specify connector geometries (use get3DPyramidConnector as a guide)
 
 
--- scripts/vfaceTester.m
+++ scripts/vfaceTester.m
@@ -1,5 +1,5 @@
 clear all
 
-DesignParams.aspectRatio=1/4;
-DesignParams.rotationCenter=1/2;
+DesignParams.aspectRatio=1/2;
+DesignParams.rotationCenter=-1.31;
 [aa,onaa,onaaCell,ndMetrics]=findWsVFaceDepth(100,DesignParams,0.1);
